5e4e4b4b4f77d11ffb23deb4fb2f14dda815196e
Jason Law <jaylaw81@gmail.com>
2026-05-30 13:23:44 -0400

    added facebook

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -40,10 +40,23 @@ export default function Footer() {
             </Link>
           </nav>
 
-          {/* Copyright */}
-          <p className="text-xs shrink-0" style={{ color: '#a78bfa' }}>
-            © {new Date().getFullYear()} StoryQuestor
-          </p>
+          {/* Social + Copyright */}
+          <div className="flex items-center gap-3 shrink-0">
+            <a
+              href="https://www.facebook.com/StoryQuestor"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="StoryQuestor on Facebook"
+              className="transition-opacity hover:opacity-70"
+            >
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="#a78bfa" opacity="0.45" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.413c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.235 2.686.235v2.97h-1.514c-1.491 0-1.956.932-1.956 1.887v2.266h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+              </svg>
+            </a>
+            <p className="text-xs" style={{ color: '#a78bfa' }}>
+              © {new Date().getFullYear()} StoryQuestor
+            </p>
+          </div>
         </div>
       </div>
     </footer>
